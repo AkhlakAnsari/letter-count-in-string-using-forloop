2589234748cef8903284bc0dce63592801153f76
AKHLAK ANSARI <87934721+AkhlakAnsari@users.noreply.github.com>
2021-09-09 08:23:57 +0530

    Add files via upload

---
+++ letter count in string using for loop.py
@@ -0,0 +1,8 @@
+#letter count in string using forloop
+string='akhlak ansari'
+count=0
+for letter in string:
+    if letter=='k':
+        count=count+1
+print(count,'letter found in string')
+# thanks akhlakansari94@gmail.com
